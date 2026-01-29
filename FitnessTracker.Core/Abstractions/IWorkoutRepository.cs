047e3649cccf28d466c319cf1d7ac5f2b4f07d04
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-01-29 13:23:03 +0300

    feat(domain): implement repository interfaces

---
+++ FitnessTracker.Core/Abstractions/IWorkoutRepository.cs
@@ -0,0 +1,13 @@
+﻿using FitnessTracker.Core.Entities;
+using FitnessTracker.Core.Models;
+
+namespace FitnessTracker.Core.Abstractions;
+
+public interface IWorkoutRepository
+{
+    Task<WorkoutEntity?> GetByIdAsync(string id, CancellationToken ct);
+    Task<List<WorkoutEntity>> GetByUserIdAsync(string userId, WorkoutFilter filter, CancellationToken ct);
+    Task<string> AddAsync(WorkoutEntity workout, CancellationToken ct);
+    Task<string> UpdateAsync(string id, WorkoutUpdateModel model, CancellationToken ct);
+    Task<string> DeleteAsync(string id, CancellationToken ct);
+}
